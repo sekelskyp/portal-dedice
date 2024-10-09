303dccb984990a74fcc28e6f009a587b320757c2
David Poslušný <david.poslusny@email.cz>
2024-10-09 18:47:18 +0200

    Footer - první verze

--- frontend/src/shared/layout/Footer.tsx
+++ frontend/src/shared/layout/Footer.tsx
@@ -1,12 +1,81 @@
-import { Box, Container, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Container,
+  Heading,
+  Link,
+  List,
+  ListItem,
+  Stack,
+} from '@chakra-ui/react'
+
+const footerLinks = [
+  {
+    id: 1,
+    title: 'Lorem Ipsum',
+    link: '#',
+  },
+  {
+    id: 2,
+    title: 'Lorem Ipsum',
+    link: '#',
+  },
+  {
+    id: 3,
+    title: 'Lorem Ipsum',
+    link: '#',
+  },
+]
+
+const FooterList = ({
+  title,
+  links,
+}: {
+  title: string
+  links: { id: number; title: string; link: string }[]
+}) => {
+  return (
+    <Stack direction="column">
+      <Heading as="h2" size="h2">
+        {title}
+      </Heading>
+      <List textAlign="center">
+        {links.map((link) => (
+          <ListItem key={link.id}>
+            <Link href={link.link}>{link.title}</Link>
+          </ListItem>
+        ))}
+      </List>
+    </Stack>
+  )
+}
 
 export const Footer = () => {
   return (
-    <Box>
+    <Box bg="gray.300">
       <Container maxW="container.xl">
-        <Text fontSize="sm" my={2}>
-          © 2023 Portál dědice. Všechna práva vyhrazena.
-        </Text>
+        <Stack
+          direction="column"
+          spacing={2}
+          align="center"
+          justify="space-between"
+          pt={4}
+        >
+          <Stack direction={{ base: 'column', md: 'row' }} spacing={12} pt={2}>
+            {footerLinks.map((footerLink) => (
+              <FooterList
+                key={footerLink.id}
+                title={footerLink.title}
+                links={footerLinks}
+              />
+            ))}
+          </Stack>
+          <Heading as="h4" size="h4" m={4} textAlign="center">
+            2024 by VŠE, Applifting
+          </Heading>
+          <Heading as="h4" size="h4" mb={4} textAlign="center">
+            © 2024 Portál dědice. Všechna práva vyhrazena.
+          </Heading>
+        </Stack>
       </Container>
     </Box>
   )
