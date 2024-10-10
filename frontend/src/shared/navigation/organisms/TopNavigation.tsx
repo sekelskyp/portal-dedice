3ce083da5d6c219333439d61a9b030026eb057f5
kubs09 <kubs09@vse.cz>
2024-10-10 17:57:06 +0200

    responzivita headru

--- frontend/src/shared/navigation/organisms/TopNavigation.tsx
+++ frontend/src/shared/navigation/organisms/TopNavigation.tsx
@@ -1,4 +1,4 @@
-import { Heading, Image, Menu, MenuButton, MenuList } from '@chakra-ui/react'
+import { Heading, Image, Menu, MenuGroup, MenuButton, MenuList, useMediaQuery } from '@chakra-ui/react'
 
 import { useAuth } from '@frontend/modules/auth'
 import { route } from '@frontend/route'
@@ -7,30 +7,63 @@ import { Button, Stack } from '@frontend/shared/design-system'
 import { RouterNavLink } from '../atoms'
 
 export function TopNavigation() {
+  const [isMobile] = useMediaQuery("(max-width: 768px)")
+
   const { user, signOut } = useAuth()
+
   return (
-    <Stack direction="row" spacing="0" alignItems="center" color="primary.900">
-      <RouterNavLink to={route.home()}>Home</RouterNavLink>
-      <RouterNavLink to={route.guide()}>How to use</RouterNavLink>
-      <RouterNavLink to={route.about()}>About us</RouterNavLink>
-      <RouterNavLink to={route.blog()}>Blog</RouterNavLink>
-      {user === null ? (
+    <Stack 
+    direction="row" 
+    spacing="0" 
+    alignItems="center" 
+    color="primary.900"
+    >
+      {!isMobile && (
         <>
+          <RouterNavLink to={route.home()}>Home</RouterNavLink>
+          <RouterNavLink to={route.guide()}>How to use</RouterNavLink>
+          <RouterNavLink to={route.about()}>About us</RouterNavLink>
+          <RouterNavLink to={route.blog()}>Blog</RouterNavLink>
+        </>
+      )}
+      {isMobile && (
         <Menu>
           <MenuButton 
-          px="4"
+          px="4" 
           py="3" 
-          fontSize="sm"
+          fontSize="sm" 
           ml="2"
-          _hover={{
-            bg: 'blackAlpha.400',
-          }}
-          _activeLink={{
-            bg: 'blackAlpha.300',
-            _hover: {
-              bg: 'blackAlpha.400',
-            },
-          }}
+          >
+            Menu
+          </MenuButton>
+          <MenuList>
+          <MenuGroup title='Login'>
+            {user === null ? (
+              <>
+                <RouterNavLink to={route.signIn()}>Sign In</RouterNavLink>
+                <RouterNavLink to={route.signUp()}>Sign Up</RouterNavLink>
+              </>
+            ) : (
+              <Button onClick={() => signOut()}>Sign Out</Button>
+            )}
+            </MenuGroup>
+            <MenuGroup title='Help'>
+              <RouterNavLink to={route.guide()}>How to use</RouterNavLink>
+              <RouterNavLink to={route.about()}>About us</RouterNavLink>
+              <RouterNavLink to={route.blog()}>Blog</RouterNavLink>
+            </MenuGroup>
+          </MenuList>
+        </Menu>
+      )}
+      {!isMobile && user === null && (
+        <Menu>
+          <MenuButton
+            px="4"
+            py="3"
+            fontSize="sm"
+            ml="2"
+            _hover={{ bg: 'blackAlpha.400' }}
+            _activeLink={{ bg: 'blackAlpha.300', _hover: { bg: 'blackAlpha.400' } }}
           >
             Login
           </MenuButton>
@@ -39,10 +72,10 @@ export function TopNavigation() {
             <RouterNavLink to={route.signUp()}>Sign Up</RouterNavLink>
           </MenuList>
         </Menu>
-        </>
-      ) : (
+      )}
+      {!isMobile && user !== null && (
         <Button ml="2" onClick={() => signOut()}>
-          Sign Out
+          Sign Out 
         </Button>
       )}
     </Stack>
