0671304fc4222fdb85df20f42a375660ab1d4625
Michal Dub <dubm07@vse.cz>
2024-10-11 14:53:08 +0200

    Úprava HomePage pro přihlášené uživatele.

--- frontend/src/modules/home/pages/HomePage.tsx
+++ frontend/src/modules/home/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { Heading, Stack, Text } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 
+import { useAuth } from '@frontend/modules/auth'
 import { route } from '@frontend/route'
 import { Button } from '@frontend/shared/design-system'
 import { SimpleCentered } from '@frontend/shared/design-system/atoms/CTA/SimpleCentered'
@@ -8,8 +9,15 @@ import { SplitWithImage } from '@frontend/shared/design-system/atoms/CTA/SplitWi
 import { Page } from '@frontend/shared/layout'
 
 export function HomePage() {
+  const { user } = useAuth()
+
   return (
     <Page as={Stack} gap={10} justifyContent={'space-between'} h={'full'}>
+      {user && (
+        <SimpleCentered>
+          <Heading>Vítejte, {user.name}!</Heading>
+        </SimpleCentered>
+      )}
       <SplitWithImage
         imageSrc="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80"
         imageAlt="Lidé hledající pomoc"
@@ -20,16 +28,18 @@ export function HomePage() {
           Pojďme na to!
         </Button>
       </SplitWithImage>
-      <SimpleCentered>
-        <Heading>Řešíte předběžné šetření?</Heading>
-        <Text>
-          Komunikujte s notářem a ostatními dědici v řešení pro předběžné
-          šetření. Zaregistujte se a získejte přístup k nástroji.
-        </Text>
-        <Button as={Link} size="lg" to={route.signUp()}>
-          Vyřešit online
-        </Button>
-      </SimpleCentered>
+      {!user && (
+        <SimpleCentered>
+          <Heading>Řešíte předběžné šetření?</Heading>
+          <Text>
+            Komunikujte s notářem a ostatními dědici v řešení pro předběžné
+            šetření. Zaregistujte se a získejte přístup k nástroji.
+          </Text>
+          <Button as={Link} size="lg" to={route.signUp()}>
+            Vyřešit online
+          </Button>
+        </SimpleCentered>
+      )}
     </Page>
   )
 }
