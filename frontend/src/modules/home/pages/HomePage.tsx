59ca24ec074d9d21b180e2dfccce85f845dc65e0
Peter Sekelsky <sekp02@vse.cz>
2024-10-13 21:36:32 +0200

    Oprava chyby na homepage

--- frontend/src/modules/home/pages/HomePage.tsx
+++ frontend/src/modules/home/pages/HomePage.tsx
@@ -15,7 +15,7 @@ export function HomePage() {
     <Page as={Stack} gap={10} justifyContent={'space-between'} h={'full'}>
       {user && (
         <SimpleCentered bg="none">
-          <Heading size="3xl">Vítejte, {user.name}!</Heading>
+          <Heading size="3xl">Vítejte, {user.login}!</Heading>
         </SimpleCentered>
       )}
       <SplitWithImage
