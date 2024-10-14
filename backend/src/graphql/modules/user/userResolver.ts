b8017ce013eb465ccd319bc19996a0b3bb35ecef
Peter Sekelsky <sekp02@vse.cz>
2024-10-14 22:58:26 +0200

    Odstranenie zbytocneho console.log

--- backend/src/graphql/modules/user/userResolver.ts
+++ backend/src/graphql/modules/user/userResolver.ts
@@ -51,8 +51,6 @@ export class UserResolver {
       .from(user)
       .where(eq(user.contactId, contactRecord[0].id))
 
-    console.log(userRecord.length)
-
     if (userRecord.length === 0) {
       throw new GraphQLError('Unauthorized.')
     }
