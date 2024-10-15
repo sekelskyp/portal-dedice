ed1db68112e43557d4dd90eb84d5ff34de3f84ba
kubs09 <kubs09@vse.cz>
2024-10-15 23:00:36 +0200

    přidání questions.json + dynamické vykreslování FAQ stránek

--- frontend/src/modules/wizard/components/QuestionStep.tsx
+++ frontend/src/modules/wizard/components/QuestionStep.tsx
@@ -1,18 +1,26 @@
 import { Box, Container, Heading, Stack } from '@chakra-ui/react'
 
+import { AccordionHelper } from './AccordionHelper'
+
 export type WizardStepxProps = {
   heading: string
   progress: number
+  questions?: { title: string; description: string }[]
 }
 
-export function QuestionStep({ heading, progress }: WizardStepxProps) {
+export function QuestionStep({
+  heading,
+  progress,
+  questions = [],
+}: WizardStepxProps) {
   return (
     <Box>
       <Stack spacing={4} alignItems="center">
         <Container maxWidth="container.sm">
           <Heading as={'h3'} size="lg" py={6} px={12} textAlign={'center'}>
-            {heading}: Otázka: {progress}%
+            {heading}
           </Heading>
+          <AccordionHelper items={questions} />
         </Container>
         <Stack direction={'row'}>{progress}...</Stack>
       </Stack>
--- frontend/src/modules/wizard/pages/WizardStepPage.tsx
+++ frontend/src/modules/wizard/pages/WizardStepPage.tsx
@@ -5,53 +5,13 @@ import { QuestionStep } from '../components/QuestionStep'
 import { StepperProgress } from '../components/StepperProgress'
 import { TestatorIdentification } from '../components/TestatorIdentification'
 import { useWizardSteps } from '../hooks/useWizardSteps'
+import questionData from '../questions.json'
 
 export function WizardPage() {
   const { step, questionsProgress, treeProgress, nextStep, previousStep } =
     useWizardSteps()
 
-  const data = [
-    {
-      id: 1,
-      heading: 'Question 1',
-    },
-    {
-      id: 2,
-      heading: 'Question 2',
-    },
-    {
-      id: 3,
-      heading: 'Question 3',
-    },
-    {
-      id: 4,
-      heading: 'Question 4',
-    },
-    {
-      id: 5,
-      heading: 'Question 5',
-    },
-    {
-      id: 6,
-      heading: 'Question 6',
-    },
-    {
-      id: 7,
-      heading: 'Question 7',
-    },
-    {
-      id: 8,
-      heading: 'Question 8',
-    },
-    {
-      id: 9,
-      heading: 'Question 9',
-    },
-    {
-      id: 10,
-      heading: 'Question 10',
-    },
-  ]
+  const data = questionData
 
   return (
     <Box width="60%" mx="auto" mt="4">
@@ -76,22 +36,22 @@ export function WizardPage() {
               <QuestionStep
                 progress={questionsProgress}
                 heading={data[questionsProgress / 10].heading}
+                questions={data[questionsProgress / 10].question}
               />
             )}
+            <Box display="flex" justifyContent="space-between" mt="8" mb="8">
+              <Button onClick={previousStep}>Zpět</Button>
+              <Button onClick={nextStep}>
+                {data[questionsProgress / 10].button}
+              </Button>
+            </Box>
           </Box>
         )}
+
         {step === 3 && <Text fontSize="lg">Průvodce řízením (Step 3)</Text>}
         {step === 4 && (
           <Text fontSize="lg">Konec, tady máte výsledek nachytřovadla.</Text>
         )}
-      </Box>
-      <Box display="flex" justifyContent="space-between" mt="8">
-        <Button onClick={previousStep} isDisabled={step === 1}>
-          Previous
-        </Button>
-        <Button onClick={nextStep} isDisabled={step === 4}>
-          Next
-        </Button>
       </Box>
     </Box>
   )
