ba9a099689aa9441c9c5fb17f0ce98a0676ee09c
Peter Sekelsky <sekp02@vse.cz>
2024-10-15 11:10:46 +0200

    Vytvorenie a pouzitie useSignIn hooku pre prihlasovanie

--- frontend/src/modules/auth/pages/SignInPage.tsx
+++ frontend/src/modules/auth/pages/SignInPage.tsx
@@ -1,37 +1,13 @@
 import { useCallback } from 'react'
-import { useMutation } from '@apollo/client'
-import { useNavigate } from 'react-router-dom'
 
-import { gql } from '@frontend/gql'
-import { useAuth } from '@frontend/modules/auth'
 import { Box } from '@frontend/shared/design-system'
 import { Page } from '@frontend/shared/layout'
 
 import { SignInForm } from '../components/SignInForm'
-
-const SIGNIN_MUTATION = gql(/* GraphQL */ `
-  mutation SignIn($email: String!, $password: String!) {
-    signIn(email: $email, password: $password) {
-      user {
-        id
-        login
-      }
-      token
-    }
-  }
-`)
+import { useSignIn } from '../hooks/useSignIn'
 
 export function SignInPage() {
-  const auth = useAuth()
-  const navigate = useNavigate()
-
-  const [signInRequest, signInRequestState] = useMutation(SIGNIN_MUTATION, {
-    onCompleted: ({ signIn: { user, token } }) => {
-      auth.signIn({ token, user })
-      navigate('/')
-    },
-    onError: () => {},
-  })
+  const [signInRequest, signInRequestState] = useSignIn()
 
   const handleSignInFormSubmit = useCallback(
     (variables: { email: string; password: string }) => {
