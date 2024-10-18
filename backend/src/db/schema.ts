19e353250f5134614d41b7b7ef4d5a101258c76b
nura16 <nura16@vse.cz>
2024-10-18 18:32:20 +0200

    pridani cizich klicu

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,3 +1,4 @@
+import { table } from 'console'
 import { SQL, sql } from 'drizzle-orm'
 import {
   AnyMySqlColumn,
@@ -5,6 +6,7 @@ import {
   char,
   date,
   float,
+  foreignKey,
   int,
   mysqlTable,
   text,
@@ -18,7 +20,9 @@ export const user = mysqlTable(
   'User',
   {
     id: int('id').primaryKey().autoincrement(),
-    contactId: int('contactId').notNull(),
+    contactId: int('contactId')
+      .references(() => contact.id)
+      .notNull(),
     login: varchar('login', { length: 255 }).notNull(),
     password: varchar('password', { length: 255 }).notNull(),
   },
@@ -47,8 +51,12 @@ export const contact = mysqlTable(
 // Define Notary Table
 export const notary = mysqlTable('Notary', {
   id: int('id').primaryKey().autoincrement(),
-  businessContactId: int('businessContactId').notNull(),
-  userId: int('userId').notNull(),
+  businessContactId: int('businessContactId')
+    .references(() => contact.id)
+    .notNull(),
+  userId: int('userId')
+    .references(() => user.id)
+    .notNull(),
 })
 
 // Define TaskState Table
@@ -66,15 +74,21 @@ export const procedureState = mysqlTable('ProcedureState', {
 // Define InheritanceProcedure Table with Foreign Key to ProcedureState
 export const inheritanceProcedure = mysqlTable('InheritanceProcedure', {
   id: int('id').primaryKey().autoincrement(),
-  notaryId: int('notaryId').notNull(),
-  stateId: int('stateId').notNull(), // FK to ProcedureState
+  notaryId: int('notaryId')
+    .references(() => notary.id)
+    .notNull(),
+  stateId: int('stateId').references(() => procedureState.id), // FK to ProcedureState
 })
 
 // Define Beneficiary Table
 export const beneficiary = mysqlTable('Beneficiary', {
   id: int('id').primaryKey().autoincrement(),
-  userId: int('userId').notNull(),
-  deceasedRelationId: int('deceasedRelationId').notNull(),
+  userId: int('userId')
+    .references(() => user.id)
+    .notNull(),
+  deceasedRelationId: int('deceasedRelationId')
+    .references(() => beneficiaryDeceasedRelation.id)
+    .notNull(),
 })
 
 // Define BeneficiaryDeceasedRelation Table (only define once)
@@ -100,7 +114,9 @@ export const meeting = mysqlTable('Meeting', {
 // Define Asset Table
 export const asset = mysqlTable('Asset', {
   id: int('id').primaryKey().autoincrement(),
-  inheritanceProcedureId: int('inheritanceProcedureId').notNull(),
+  inheritanceProcedureId: int('inheritanceProcedureId')
+    .references(() => inheritanceProcedure.id)
+    .notNull(),
   value: float('value').notNull(),
   name: varchar('name', { length: 100 }).notNull(),
   description: text('description'),
@@ -109,8 +125,10 @@ export const asset = mysqlTable('Asset', {
 // Define Document Table
 export const document = mysqlTable('Document', {
   id: int('id').primaryKey().autoincrement(),
-  inheritanceProcedureId: int('inheritanceProcedureId').notNull(),
-  taskId: int('taskId'),
+  inheritanceProcedureId: int('inheritanceProcedureId')
+    .references(() => inheritanceProcedure.id)
+    .notNull(),
+  taskId: int('taskId').references(() => task.id),
   userOwnerId: int('userOwnerId'),
   createDate: timestamp('createDate').defaultNow(),
   fileName: varchar('fileName', { length: 255 }).notNull(),
@@ -121,26 +139,21 @@ export const document = mysqlTable('Document', {
 // Define Chat Table
 export const chat = mysqlTable('Chat', {
   id: int('id').primaryKey().autoincrement(),
-  inheritanceProcedureId: int('inheritanceProcedureId').notNull(),
+  inheritanceProcedureId: int('inheritanceProcedureId')
+    .references(() => inheritanceProcedure.id)
+    .notNull(),
 })
 
 // Define ChatMessage Table
 export const chatMessage = mysqlTable('ChatMessage', {
   id: int('id').primaryKey().autoincrement(),
-  chatId: int('chatId').notNull(),
-  userId: int('userId').notNull(),
+  chatId: int('chatId')
+    .references(() => chat.id)
+    .notNull(),
+  userId: int('userId')
+    .references(() => user.id)
+    .notNull(),
   body: text('body').notNull(),
-})
-
-// Define Task Table with Foreign Key to TaskState
-export const task = mysqlTable('Task', {
-  id: int('id').primaryKey().autoincrement(),
-  typeId: int('typeId').notNull(),
-  deadline: date('deadline'),
-  stateId: int('stateId').notNull(), // FK to TaskState
-  label: varchar('label', { length: 100 }).notNull(),
-  description: text('description'),
-  inheritanceProcedureId: int('inheritanceProcedureId').notNull(),
 })
 
 // Define TaskType Table
@@ -150,20 +163,41 @@ export const taskType = mysqlTable('TaskType', {
   description: text('description'),
 })
 
+// Define Task Table with Foreign Key to TaskState
+export const task = mysqlTable('Task', {
+  id: int('id').primaryKey().autoincrement(),
+  typeId: int('typeId')
+    .references(() => taskType.id)
+    .notNull(),
+  deadline: date('deadline'),
+  stateId: int('stateId')
+    .references(() => taskState.id)
+    .notNull(), // FK to TaskState
+  label: varchar('label', { length: 100 }).notNull(),
+  description: text('description'),
+  inheritanceProcedureId: int('inheritanceProcedureId')
+    .references(() => inheritanceProcedure.id)
+    .notNull(),
+})
+
 // Define FAQ Table
 export const faq = mysqlTable('FAQ', {
   id: int('id').primaryKey().autoincrement(),
   question: varchar('question', { length: 100 }).notNull(),
   answer: varchar('answer', { length: 255 }).notNull(),
   createDate: timestamp('createDate').defaultNow(),
-  notaryOwnerId: int('notaryOwnerId'),
-  taskTypeId: int('taskTypeId'),
+  notaryOwnerId: int('notaryOwnerId').references(() => notary.id),
+  taskTypeId: int('taskTypeId').references(() => taskType.id),
 })
 
 // Define BeneficiaryMeetingRel Table for M2M between Beneficiary and Meeting
 export const beneficiaryMeetingRel = mysqlTable('BeneficiaryMeetingRel', {
-  beneficiaryId: int('beneficiaryId').notNull(),
-  meetingId: int('meetingId').notNull(),
+  beneficiaryId: int('beneficiaryId')
+    .references(() => beneficiary.id)
+    .notNull(),
+  meetingId: int('meetingId')
+    .references(() => meeting.id)
+    .notNull(),
 })
 
 // Define BeneficiaryInheritanceProcedureRel Table for M2M between Beneficiary and InheritanceProcedure
@@ -172,13 +206,31 @@ export const beneficiaryInheritanceProcedureRel = mysqlTable(
   {
     beneficiaryId: int('beneficiaryId').notNull(),
     inheritanceProcedureId: int('inheritanceProcedureId').notNull(),
-  }
+  },
+  (table) => ({
+    cfk: [
+      foreignKey({
+        name: 'BIPRel_inhPId_InheritanceProcedure_id_fk',
+        columns: [table.inheritanceProcedureId],
+        foreignColumns: [inheritanceProcedure.id],
+      }),
+      foreignKey({
+        name: 'BIPRel_benId_Beneficiary_id_fk',
+        columns: [table.beneficiaryId],
+        foreignColumns: [beneficiary.id],
+      }),
+    ],
+  })
 )
 
 // Define BeneficiaryTaskRel Table for M2M between Beneficiary and Task
 export const beneficiaryTaskRel = mysqlTable('BeneficiaryTaskRel', {
-  beneficiaryId: int('beneficiaryId').notNull(),
-  taskId: int('taskId').notNull(),
+  beneficiaryId: int('beneficiaryId')
+    .references(() => beneficiary.id)
+    .notNull(),
+  taskId: int('taskId')
+    .references(() => task.id)
+    .notNull(),
 })
 
 // Custom lower function
--- backend/src/db/seed.ts
+++ backend/src/db/seed.ts
@@ -1,28 +1,41 @@
-import { sql } from 'drizzle-orm'
+// import { not, sql } from 'drizzle-orm'
 
 import { getConnection } from './db'
+import { beneficiary, contact, notary, user } from './schema'
 
 async function seed() {
   const { db, connection } = await getConnection()
 
   try {
+
+    await db.delete(contact);
+    await db.delete(user);
+    await db.delete(beneficiary);
+    await db.delete(notary);
+
+
+    
+
+
+
+
     // Use INSERT IGNORE to insert task states, ignoring duplicates
-    await db.execute(
-      sql`INSERT IGNORE INTO TaskState (state) VALUES 
-      ('Open'), ('InProgress'), ('Completed'), ('Closed')`
-    )
+    // await db.execute(
+    //   sql`INSERT IGNORE INTO TaskState (state) VALUES 
+    //   ('Open'), ('InProgress'), ('Completed'), ('Closed')`
+    // )
 
-    // Use INSERT IGNORE to insert procedure states, ignoring duplicates
-    await db.execute(
-      sql`INSERT IGNORE INTO ProcedureState (state) VALUES 
-      ('Initiated'), ('Pending'), ('Resolved'), ('Closed')`
-    )
+    // // Use INSERT IGNORE to insert procedure states, ignoring duplicates
+    // await db.execute(
+    //   sql`INSERT IGNORE INTO ProcedureState (state) VALUES 
+    //   ('Initiated'), ('Pending'), ('Resolved'), ('Closed')`
+    // )
 
-    // Use INSERT IGNORE to insert beneficiary deceased relation, ignoring duplicates
-    await db.execute(
-      sql`INSERT IGNORE INTO ProcedureState (state) VALUES 
-        ('Spouse'), ('Child'), ('Parent'), ('Other')`
-    )
+    // // Use INSERT IGNORE to insert beneficiary deceased relation, ignoring duplicates
+    // await db.execute(
+    //   sql`INSERT IGNORE INTO ProcedureState (state) VALUES 
+    //     ('Spouse'), ('Child'), ('Parent'), ('Other')`
+    // )
 
     console.log('Database seeded successfully.')
   } catch (error) {
