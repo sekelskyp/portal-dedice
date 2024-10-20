9c774326def0bece295f547722810e6b9fc675dc
Peter Sekelsky <sekp02@vse.cz>
2024-10-21 00:35:21 +0200

    Odstranenie fragmentu

--- frontend/src/modules/wizard/components/TestatorIdentification.tsx
+++ frontend/src/modules/wizard/components/TestatorIdentification.tsx
@@ -27,48 +27,47 @@ export function TestatorIdentification({ nextStep }: NextStepProps) {
   }
 
   return (
-    <>
-      <Form onSubmit={onSubmit} resolver={zodResolver(schema)} noValidate>
-        <Container
-          maxW="container.xl"
-          width="100%"
-          px={{ base: 4, sm: 8, md: 12, lg: 16 }}
-          py={{ base: 2, sm: 4 }}
+    <Form onSubmit={onSubmit} resolver={zodResolver(schema)} noValidate>
+      <Container
+        maxW="container.xl"
+        width="100%"
+        px={{ base: 4, sm: 8, md: 12, lg: 16 }}
+        py={{ base: 2, sm: 4 }}
+      >
+        <Text fontSize={{ base: 'sm', sm: 'md', md: 'lg' }} pb={4}>
+          Vyplněním formuláře údaji zůstavitele Vám pomůžeme zjistit, který
+          notář bude spravovat Vaše pozůstalostní řízení.
+        </Text>
+        <Stack
+          gap={5}
+          p={8}
+          borderWidth="2px"
+          borderColor="gray.100"
+          borderRadius="xl"
         >
-          <Text fontSize={{ base: 'sm', sm: 'md', md: 'lg' }} pb={4}>
-            Vyplněním formuláře údaji zůstavitele Vám pomůžeme zjistit, který
-            notář bude spravovat Vaše pozůstalostní řízení.
-          </Text>
-          <Stack
-            gap={5}
-            p={8}
-            borderWidth="2px"
-            borderColor="gray.100"
-            borderRadius="xl"
+          <RadioGroupControl
+            name="sex"
+            label="Pohlaví"
+            labelProps={{ fontSize: { base: 'sm', md: 'md' } }}
+            isRequired
           >
-            <RadioGroupControl
-              name="sex"
-              label="Pohlaví"
-              labelProps={{ fontSize: { base: 'sm', md: 'md' } }}
-              isRequired
-            >
-              <Stack direction="row" spacing={5}>
-                <Radio value="male" size={{ base: 'sm', md: 'md' }}>
-                  Muž
-                </Radio>
-                <Radio value="female" size={{ base: 'sm', md: 'md' }}>
-                  Žena
-                </Radio>
-              </Stack>
-            </RadioGroupControl>
-            <InputControl
-              name="birthDate"
-              label="Datum narození"
-              labelProps={{ fontSize: { base: 'sm', md: 'md' } }}
-              inputProps={{ type: 'date', fontSize: { base: 'sm', md: 'md' } }}
-              isRequired
-            ></InputControl>
-            {/*
+            <Stack direction="row" spacing={5}>
+              <Radio value="male" size={{ base: 'sm', md: 'md' }}>
+                Muž
+              </Radio>
+              <Radio value="female" size={{ base: 'sm', md: 'md' }}>
+                Žena
+              </Radio>
+            </Stack>
+          </RadioGroupControl>
+          <InputControl
+            name="birthDate"
+            label="Datum narození"
+            labelProps={{ fontSize: { base: 'sm', md: 'md' } }}
+            inputProps={{ type: 'date', fontSize: { base: 'sm', md: 'md' } }}
+            isRequired
+          ></InputControl>
+          {/*
             <InputControl
             name="address"
             label="Trvalé bydliště"
@@ -77,14 +76,13 @@ export function TestatorIdentification({ nextStep }: NextStepProps) {
             isRequired
             ></InputControl>
             */}
-            <PlacesAutoComplete name="address" label="Trvalé bydliště" />
-            <Spacer></Spacer>
-            <Box>
-              <SubmitButton>Potvrdit údaje</SubmitButton>
-            </Box>
-          </Stack>
-        </Container>
-      </Form>
-    </>
+          <PlacesAutoComplete name="address" label="Trvalé bydliště" />
+          <Spacer></Spacer>
+          <Box>
+            <SubmitButton>Potvrdit údaje</SubmitButton>
+          </Box>
+        </Stack>
+      </Container>
+    </Form>
   )
 }
