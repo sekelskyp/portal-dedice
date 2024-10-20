3a75ffebeb08334172626a07c108959e3c1fcd36
Peter Sekelsky <sekp02@vse.cz>
2024-10-20 16:31:35 +0200

    Oprava rimraf a formatovania

--- frontend/src/gql/fragment-masking.ts
+++ frontend/src/gql/fragment-masking.ts
@@ -8,14 +8,15 @@ import { FragmentDefinitionNode } from 'graphql'
 import { Incremental } from './graphql'
 
 export type FragmentType<
-  TDocumentType extends DocumentTypeDecoration<any, any>
-> = TDocumentType extends DocumentTypeDecoration<infer TType, any>
-  ? [TType] extends [{ ' $fragmentName'?: infer TKey }]
-    ? TKey extends string
-      ? { ' $fragmentRefs'?: { [key in TKey]: TType } }
+  TDocumentType extends DocumentTypeDecoration<any, any>,
+> =
+  TDocumentType extends DocumentTypeDecoration<infer TType, any>
+    ? [TType] extends [{ ' $fragmentName'?: infer TKey }]
+      ? TKey extends string
+        ? { ' $fragmentRefs'?: { [key in TKey]: TType } }
+        : never
       : never
     : never
-  : never
 
 // return non-nullable if `fragmentType` is non-nullable
 export function useFragment<TType>(
@@ -80,7 +81,7 @@ export function useFragment<TType>(
 
 export function makeFragmentData<
   F extends DocumentTypeDecoration<any, any>,
-  FT extends ResultOf<F>
+  FT extends ResultOf<F>,
 >(data: FT, _fragment: F): FragmentType<F> {
   return data as FragmentType<F>
 }
