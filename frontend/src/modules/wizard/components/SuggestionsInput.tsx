3c212428d053c33ab0899486d2924f9301e8fbb5
nura16 <nura16@vse.cz>
2024-10-20 14:52:06 +0200

    pridani naseptavac adresy

---
+++ frontend/src/modules/wizard/components/SuggestionsInput.tsx
@@ -0,0 +1,85 @@
+import useOnclickOutside from 'react-cool-onclickoutside'
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from 'use-places-autocomplete'
+
+export const PlacesAutocomplete = () => {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    callbackName: 'initMap',
+    requestOptions: {
+      /* Define search scope here */
+    },
+    debounce: 300,
+  })
+  const ref = useOnclickOutside(() => {
+    // When the user clicks outside of the component, we can dismiss
+    // the searched suggestions by calling this method
+    clearSuggestions()
+  })
+
+  const handleInput = (e: any) => {
+    // Update the keyword of the input element
+    setValue(e.target.value)
+  }
+
+  const handleSelect =
+    ({ description }: { description: string }) =>
+    () => {
+      // When the user selects a place, we can replace the keyword without request data from API
+      // by setting the second parameter to "false"
+      setValue(description, false)
+      clearSuggestions()
+
+      // Get latitude and longitude via utility functions
+      getGeocode({ address: description }).then((results) => {
+        const { lat, lng } = getLatLng(results[0])
+        console.log('📍 Coordinates: ', { lat, lng })
+      })
+    }
+
+  const renderSuggestions = () =>
+    data.map((suggestion) => {
+      const {
+        place_id,
+        structured_formatting: { main_text, secondary_text },
+      } = suggestion
+
+      return (
+        <li key={place_id} onClick={handleSelect(suggestion)}>
+          <strong>{main_text}</strong> <small>{secondary_text}</small>
+        </li>
+      )
+    })
+
+  console.log(ready)
+
+  return (
+    <>
+      {/* <script
+        defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBNWbMJzrf02XhtdcIG7IeGnJYdZuLSkFE&libraries=places&callback=initMap"
+      ></script> */}
+      <script
+        async
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBNWbMJzrf02XhtdcIG7IeGnJYdZuLSkFE&loading=async&libraries=places&callback=initMap"
+      ></script>
+      <div ref={ref}>
+        <input
+          value={value}
+          onChange={handleInput}
+          // disabled={!ready}
+          placeholder="Where are you going?"
+        />
+        {/* We can use the "status" to decide whether we should display the dropdown or not */}
+        {status === 'OK' && <ul>{renderSuggestions()}</ul>}
+      </div>
+    </>
+  )
+}
--- frontend/src/modules/wizard/components/TestatorIdentification.tsx
+++ frontend/src/modules/wizard/components/TestatorIdentification.tsx
@@ -9,6 +9,8 @@ import { z } from 'zod'
 
 import { Form } from '../../../shared/forms/Form'
 
+import { PlacesAutocomplete } from './SuggestionsInput'
+
 const schema = z.object({
   sex: z.string().min(1, 'Pohlaví je povinné.'),
   birthDate: z.string().min(1, 'Datum narození je povinné.'),
@@ -25,59 +27,63 @@ export function TestatorIdentification({ nextStep }: NextStepProps) {
   }
 
   return (
-    <Form onSubmit={onSubmit} resolver={zodResolver(schema)} noValidate>
-      <Container
-        maxW="container.xl"
-        width="100%"
-        px={{ base: 4, sm: 8, md: 12, lg: 16 }}
-        py={{ base: 2, sm: 4 }}
-      >
-        <Text fontSize={{ base: 'sm', sm: 'md', md: 'lg' }} pb={4}>
-          Vyplněním formuláře údaji zůstavitele Vám pomůžeme zjistit, který
-          notář bude spravovat Vaše pozůstalostní řízení.
-        </Text>
-        <Stack
-          gap={5}
-          p={8}
-          borderWidth="2px"
-          borderColor="gray.100"
-          borderRadius="xl"
+    <>
+      <Form onSubmit={onSubmit} resolver={zodResolver(schema)} noValidate>
+        <Container
+          maxW="container.xl"
+          width="100%"
+          px={{ base: 4, sm: 8, md: 12, lg: 16 }}
+          py={{ base: 2, sm: 4 }}
         >
-          <RadioGroupControl
-            name="sex"
-            label="Pohlaví"
-            labelProps={{ fontSize: { base: 'sm', md: 'md' } }}
-            isRequired
+          <Text fontSize={{ base: 'sm', sm: 'md', md: 'lg' }} pb={4}>
+            Vyplněním formuláře údaji zůstavitele Vám pomůžeme zjistit, který
+            notář bude spravovat Vaše pozůstalostní řízení.
+          </Text>
+          <Stack
+            gap={5}
+            p={8}
+            borderWidth="2px"
+            borderColor="gray.100"
+            borderRadius="xl"
           >
-            <Stack direction="row" spacing={5}>
-              <Radio value="male" size={{ base: 'sm', md: 'md' }}>
-                Muž
-              </Radio>
-              <Radio value="female" size={{ base: 'sm', md: 'md' }}>
-                Žena
-              </Radio>
-            </Stack>
-          </RadioGroupControl>
-          <InputControl
-            name="birthDate"
-            label="Datum narození"
-            labelProps={{ fontSize: { base: 'sm', md: 'md' } }}
-            inputProps={{ type: 'date', fontSize: { base: 'sm', md: 'md' } }}
-            isRequired
-          ></InputControl>
-          <InputControl
-            name="address"
-            label="Trvalé bydliště"
-            labelProps={{ fontSize: { base: 'sm', md: 'md' } }}
-            inputProps={{ fontSize: { base: 'sm', md: 'md' } }}
-            isRequired
-          ></InputControl>
-          <Spacer></Spacer>
-          <Box>
-            <SubmitButton>Potvrdit údaje</SubmitButton>
-          </Box>
-        </Stack>
-      </Container>
-    </Form>
+            <RadioGroupControl
+              name="sex"
+              label="Pohlaví"
+              labelProps={{ fontSize: { base: 'sm', md: 'md' } }}
+              isRequired
+            >
+              <Stack direction="row" spacing={5}>
+                <Radio value="male" size={{ base: 'sm', md: 'md' }}>
+                  Muž
+                </Radio>
+                <Radio value="female" size={{ base: 'sm', md: 'md' }}>
+                  Žena
+                </Radio>
+              </Stack>
+            </RadioGroupControl>
+            <InputControl
+              name="birthDate"
+              label="Datum narození"
+              labelProps={{ fontSize: { base: 'sm', md: 'md' } }}
+              inputProps={{ type: 'date', fontSize: { base: 'sm', md: 'md' } }}
+              isRequired
+            ></InputControl>
+            <InputControl
+              name="address"
+              label="Trvalé bydliště"
+              labelProps={{ fontSize: { base: 'sm', md: 'md' } }}
+              inputProps={{ fontSize: { base: 'sm', md: 'md' } }}
+              isRequired
+            ></InputControl>
+            <Spacer></Spacer>
+            <Box>
+              <SubmitButton>Potvrdit údaje</SubmitButton>
+            </Box>
+          </Stack>
+        </Container>
+      </Form>
+
+      <PlacesAutocomplete />
+    </>
   )
 }
