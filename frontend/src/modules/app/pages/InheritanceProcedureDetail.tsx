b781469f38ad15a99c8d218063b0894fb788393f
nura16 <nura16@vse.cz>
2024-11-02 14:57:20 +0100

    pridani CTA na modelaci

--- frontend/src/modules/app/pages/InheritanceProcedureDetail.tsx
+++ frontend/src/modules/app/pages/InheritanceProcedureDetail.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { useQuery } from '@apollo/client'
-import { Box, Heading, List, Spinner, Text } from '@chakra-ui/react'
-import { useParams } from 'react-router-dom'
+import {
+  Box,
+  Button,
+  Heading,
+  List,
+  Spinner,
+  Stack,
+  Text,
+} from '@chakra-ui/react'
+import { Link, useParams } from 'react-router-dom'
 
 import { gql } from '@frontend/gql'
 import { useAuth } from '@frontend/modules/auth'
 import { Page } from '@frontend/shared/layout'
+import { UnauthorizedPage } from '@frontend/shared/navigation/pages/UnauthorizedPage'
 
 const GET_PROCEDURE_QUERY = gql(/* GraphQL */ `
   query GetProcedureById($id: Int!) {
@@ -55,10 +64,8 @@ const GET_PROCEDURE_QUERY = gql(/* GraphQL */ `
 
 const InheritanceProcedureDetail: React.FC = () => {
   const user = useAuth()
-  console.log('user.user', user.user?.isNotary)
-  console.log('user.token', user.token)
-
   const { id } = useParams()
+
   const idInt = parseInt(id ?? '0', 10)
   const { loading, error, data } = useQuery(GET_PROCEDURE_QUERY, {
     variables: { id: idInt },
@@ -86,79 +93,105 @@ const InheritanceProcedureDetail: React.FC = () => {
     procedure?.procedureAssets?.reduce((sum, asset) => sum + asset.value, 0) ??
     0
 
-  return (
-    <Page>
-      <Box display="flex" alignItems="center">
-        <Heading as="h1">Procedure Detail</Heading>
-      </Box>
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        minH={{ base: 'xs', sm: 'container.sm' }}
-      >
-        {procedure ? (
-          <Box>
-            <Text fontSize="lg">
-              <strong>ID:</strong> {procedure.id}
-            </Text>
-            <Text fontSize="lg">
-              <strong>Řízení:</strong> {procedure.name}
-            </Text>
-            <Text fontSize="lg">
-              <strong>Hlavní kontaktní osoba:</strong>{' '}
-              {procedure.mainBeneficiary?.contact?.name}{' '}
-              {procedure.mainBeneficiary?.contact?.surname}
-            </Text>
-            <Text fontSize="lg">
-              <strong>Status:</strong> {procedure.state}
-            </Text>
-            <Heading as="h2" size="lg" mt={5} mb={3}>
-              Výpis dědiců
-            </Heading>
-            <List.Root listStyleType={'none'}>
-              {procedure.beneficiaries?.map((beneficiary) => (
-                <List.Item key={beneficiary.id}>
-                  <Text>
-                    <strong>User Email:</strong> {beneficiary.user?.email}
-                  </Text>
-                  <Text>
-                    <strong>Contact Name:</strong> {beneficiary.contact?.name}{' '}
-                    {beneficiary.contact?.surname}
-                  </Text>
-                  <Text>
-                    <strong>Contact Email:</strong> {beneficiary.contact?.email}
-                  </Text>
-                </List.Item>
-              ))}
-            </List.Root>{' '}
-            <Text fontSize="lg" mt={3}>
-              <strong>Celková hodnota majetku:</strong> {totalAssetsValue} ,- Kč
-              {/* TODO: CTA na modelaci */}
-            </Text>
-            <Heading as="h2" size="lg" mt={5} mb={3}>
-              Děděné položky
-            </Heading>
-            <List.Root listStyleType={'none'}>
-              {procedure.procedureAssets?.map((asset) => (
-                <List.Item key={asset.id}>
-                  <Text>
-                    <strong>Název:</strong> {asset.name}
-                  </Text>
-                  <Text>
-                    <strong>Hodnota:</strong> {asset.value}
-                  </Text>
-                </List.Item>
-              ))}
-            </List.Root>
-            {/* TODO: CTA na modelaci */}
-          </Box>
-        ) : (
-          <Text>No procedure found</Text>
-        )}
-      </Box>
-    </Page>
-  )
+  if (!user.token) {
+    return <UnauthorizedPage />
+  } else {
+    return (
+      <Page>
+        <Box display="flex" alignItems="center">
+          <Heading as="h1">Procedure Detail</Heading>
+        </Box>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          minH={{ base: 'xs', sm: 'container.sm' }}
+        >
+          {procedure ? (
+            <Box>
+              <Text fontSize="lg">
+                <strong>ID:</strong> {procedure.id}
+              </Text>
+              <Text fontSize="lg">
+                <strong>Řízení:</strong> {procedure.name}
+              </Text>
+              <Text fontSize="lg">
+                <strong>Hlavní kontaktní osoba:</strong>{' '}
+                {procedure.mainBeneficiary?.contact?.name}{' '}
+                {procedure.mainBeneficiary?.contact?.surname}
+              </Text>
+              <Text fontSize="lg">
+                <strong>Status:</strong> {procedure.state}
+              </Text>
+              <Heading as="h2" size="lg" mt={5} mb={3}>
+                Výpis dědiců
+              </Heading>
+              <List.Root listStyleType={'none'}>
+                {procedure.beneficiaries?.map((beneficiary) => (
+                  <List.Item key={beneficiary.id}>
+                    <Text>
+                      <strong>User Email:</strong> {beneficiary.user?.email}
+                    </Text>
+                    <Text>
+                      <strong>Contact Name:</strong> {beneficiary.contact?.name}{' '}
+                      {beneficiary.contact?.surname}
+                    </Text>
+                    <Text>
+                      <strong>Contact Email:</strong>{' '}
+                      {beneficiary.contact?.email}
+                    </Text>
+                  </List.Item>
+                ))}
+              </List.Root>{' '}
+              <Text fontSize="lg" mt={3}>
+                <strong>Celková hodnota majetku:</strong> {totalAssetsValue} ,-
+                Kč
+                {/* TODO: CTA na modelaci */}
+              </Text>
+              <Heading as="h2" size="lg" mt={5} mb={3}>
+                Děděné položky
+              </Heading>
+              <List.Root listStyleType={'none'}>
+                {procedure.procedureAssets?.map((asset) => (
+                  <List.Item key={asset.id}>
+                    <Text>
+                      <strong>Název:</strong> {asset.name}
+                    </Text>
+                    <Text>
+                      <strong>Hodnota:</strong> {asset.value}
+                    </Text>
+                  </List.Item>
+                ))}
+              </List.Root>
+              <Stack>
+                {!user.user?.isNotary ? (
+                  <>
+                    <Button as={Link} disabled>
+                      Modelace dědického vyrovnání
+                    </Button>
+                    <Button as={Link} disabled>
+                      Přiložit přílohu
+                    </Button>
+                    <Button as={Link} disabled>
+                      Chat s notářem
+                    </Button>
+                  </>
+                ) : (
+                  <>
+                    <Button as={Link} disabled>
+                      Hromadná zpráva všem zůstavitelům
+                    </Button>
+                  </>
+                )}
+              </Stack>
+            </Box>
+          ) : (
+            <Text>No procedure found</Text>
+          )}
+        </Box>
+      </Page>
+    )
+  }
 }
 
 export default InheritanceProcedureDetail
