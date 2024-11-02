2fac2672bd8a764ca90662fc0feb96d304e8631a
nura16 <nura16@vse.cz>
2024-11-02 17:43:02 +0100

    pridani rizeni bez majetku

--- backend/scripts/seed.ts
+++ backend/scripts/seed.ts
@@ -24,55 +24,70 @@ async function populateDatabase(
   console.log('Seeding population data...')
 
   // Insert contacts and save returned IDs
-  const [beneficiaryContactId1, beneficiaryContactId2, deceasedContactId1] =
-    await db
-      .insert(contact)
-      .values([
-        {
-          name: 'Young',
-          surname: 'Gatchell',
-          displayName: 'Young Gatchell',
-          gender: 'Male',
-          phone: '+420666666661',
-          email: 'gatyou@quacker.com',
-          completeAddress: 'Main Street 123, Brno, Czech Republic',
-          postalCode: '11000',
-        },
-        {
-          name: 'Petr',
-          surname: 'Hochman',
-          displayName: 'Petr Hochman',
-          gender: 'Male',
-          phone: '+420555555551',
-          email: 'hocpet@quacker.com',
-          completeAddress: 'Main Street 456, Brno, Czech Republic',
-          postalCode: '15000',
-        },
-        {
-          name: 'Alice',
-          surname: 'Novakova',
-          displayName: 'Alice Novakova',
-          gender: 'Female',
-          phone: '+420444444441',
-          email: 'novali@quacker.com',
-          completeAddress: 'Liberty Avenue 789, Prague, Czech Republic',
-          postalCode: '12000',
-        },
-        {
-          name: 'Tomas',
-          surname: 'Vesely',
-          displayName: 'Tomas Vesely',
-          gender: 'Male',
-          phone: '+420333333331',
-          email: 'vestom@quacker.com',
-          completeAddress: 'Peace Square 321, Ostrava, Czech Republic',
-          postalCode: '13000',
-        },
-      ])
-      .$returningId()
+  const [
+    beneficiaryContactId1,
+    beneficiaryContactId2,
+    deceasedContactId1,
+    beneficiaryContactId3,
+    deceasedContactId2,
+  ] = await db
+    .insert(contact)
+    .values([
+      {
+        name: 'Young',
+        surname: 'Gatchell',
+        displayName: 'Young Gatchell',
+        gender: 'Male',
+        phone: '+420666666661',
+        email: 'gatyou@quacker.com',
+        completeAddress: 'Main Street 123, Brno, Czech Republic',
+        postalCode: '11000',
+      },
+      {
+        name: 'Petr',
+        surname: 'Hochman',
+        displayName: 'Petr Hochman',
+        gender: 'Male',
+        phone: '+420555555551',
+        email: 'hocpet@quacker.com',
+        completeAddress: 'Main Street 456, Brno, Czech Republic',
+        postalCode: '15000',
+      },
+      {
+        name: 'Alice',
+        surname: 'Novakova',
+        displayName: 'Alice Novakova',
+        gender: 'Female',
+        phone: '+420444444441',
+        email: 'novali@quacker.com',
+        completeAddress: 'Liberty Avenue 789, Prague, Czech Republic',
+        postalCode: '12000',
+      },
+      {
+        name: 'Tomas',
+        surname: 'Vesely',
+        displayName: 'Tomas Vesely',
+        gender: 'Male',
+        phone: '+420333333331',
+        email: 'vestom@quacker.com',
+        completeAddress: 'Peace Square 321, Ostrava, Czech Republic',
+        postalCode: '13000',
+      },
+      {
+        name: 'Jan',
+        surname: 'Svoboda',
+        displayName: 'Jan Svoboda',
+        gender: 'Male',
+        phone: '+42033333333š',
+        email: 'svojan@quacker.com',
+        completeAddress: 'Peace Square 321, Ostrava, Czech Republic',
+        postalCode: '13000',
+      },
+    ])
+    .$returningId()
 
   // Insert users and save returned IDs
-  const [beneficiaryUserId1, beneficiaryUserId2] = await db
+  const [beneficiaryUserId1, beneficiaryUserId2, beneficiaryUserId3] = await db
     .insert(user)
     .values([
       {
@@ -82,6 +97,10 @@ async function populateDatabase(
       {
         password: await hashPassword('heslo1234'),
         email: 'test.email2@email.com',
+      },
+      {
+        password: await hashPassword('heslo1234'),
+        email: 'test.email3@email.com',
       },
     ])
     .$returningId()
@@ -102,10 +121,16 @@ async function populateDatabase(
         deceasedRelation: 'Child',
         dateOfBirth: new Date('1980-01-01'),
       },
+      {
+        userId: beneficiaryUserId3.id,
+        contactId: beneficiaryContactId3.id,
+        deceasedRelation: 'Parent',
+        dateOfBirth: new Date('1990-01-01'),
+      },
     ])
     .onDuplicateKeyUpdate({ set: { userId: beneficiaryUserId2.id } })
 
-  const [inheritanceId1] = await seedInheritanceProcedures(db, [
+  const [inheritanceId1, inheritanceId2] = await seedInheritanceProcedures(db, [
     {
       notaryId: notaryIds[0],
       mainBeneficiaryId: beneficiaryUserId1.id,
@@ -114,6 +139,15 @@ async function populateDatabase(
       deceasedContactId: deceasedContactId1.id,
       deceasedDateOfBirth: new Date('1940-01-01'),
       deceasedDateOfDeath: new Date('2023-12-31'),
+    },
+    {
+      notaryId: notaryIds[0],
+      mainBeneficiaryId: beneficiaryUserId3.id,
+      state: 'InProgress',
+      startDate: new Date('2024-03-10'),
+      deceasedContactId: deceasedContactId2.id,
+      deceasedDateOfBirth: new Date('1956-12-01'),
+      deceasedDateOfDeath: new Date('2024-03-12'),
     },
   ])
 
@@ -125,6 +159,10 @@ async function populateDatabase(
     {
       beneficiaryId: beneficiaryUserId2.id,
       inheritanceProcedureId: inheritanceId1,
+    },
+    {
+      beneficiaryId: beneficiaryUserId3.id,
+      inheritanceProcedureId: inheritanceId2,
     },
   ])
 
