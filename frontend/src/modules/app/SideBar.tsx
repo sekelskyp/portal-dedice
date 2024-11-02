edca13c52df20ad53231714e905f6d57fc267c46
Šimon Kubín <simonkubin77@gmail.com>
2024-11-02 13:52:53 +0100

    úprava responzivity portálu (sidebar)

--- frontend/src/modules/app/SideBar.tsx
+++ frontend/src/modules/app/SideBar.tsx
@@ -1,6 +1,14 @@
-import { ReactElement } from 'react'
-import { IconButton, Separator, Text, VStack } from '@chakra-ui/react'
+import React, { ReactElement } from 'react'
+import {
+  Center,
+  HStack,
+  IconButton,
+  Separator,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
 import { LuArchive, LuFolderPlus, LuSettings, LuUser2 } from 'react-icons/lu'
+import { useMediaQuery } from 'usehooks-ts'
 
 import resources from '@frontend/resources'
 import { RouterNavLink } from '@frontend/shared/navigation/atoms/RouterNavLink'
@@ -36,19 +44,39 @@ const sideBarItems: SideBarItem[] = [
 ]
 
 export default function SideBar() {
-  return (
+  const isMobile = useMediaQuery('(max-width: 425px)')
+  return !isMobile ? (
     <VStack align="left">
       {sideBarItems.map(({ to, label, icon, ...rest }) => (
-        <>
+        <React.Fragment key={to}>
           <IconButton variant="ghost" size="lg" justifyContent="start" px={4}>
             {icon}
-            <RouterNavLink variant="ghost" to={to} key={to} {...rest}>
+            <RouterNavLink variant="ghost" to={to} {...rest}>
               <Text fontSize="md">{label}</Text>
             </RouterNavLink>
           </IconButton>
           <Separator size="md" />
-        </>
+        </React.Fragment>
       ))}
     </VStack>
+  ) : (
+    <HStack align="top" justifyContent={'center'}>
+      {sideBarItems.map(({ to, label, icon, ...rest }) => (
+        <React.Fragment key={to}>
+          <IconButton
+            bg="gray.200"
+            variant="ghost"
+            size="sm"
+            justifyContent="center"
+            px={2}
+            _active={{ bg: 'gray.400' }} // Highlight when active
+          >
+            <RouterNavLink variant="ghost" to={to} {...rest}>
+              {icon}
+            </RouterNavLink>
+          </IconButton>
+        </React.Fragment>
+      ))}
+    </HStack>
   )
 }
--- frontend/src/shared/layout/PortalLayout.tsx
+++ frontend/src/shared/layout/PortalLayout.tsx
@@ -14,11 +14,11 @@ export const PortalLayout = () => {
   if (user.token) {
     return (
       <Page>
-        <Grid templateColumns="repeat(5, 1fr)" gap={12}>
+        <Grid templateColumns={{ base: '1fr', md: 'repeat(5, 1fr)' }} gap={12}>
           <GridItem colSpan={1}>
             <SideBar />
           </GridItem>
-          <GridItem colSpan={4}>
+          <GridItem colSpan={{ base: 1, md: 4 }}>
             <Outlet />
           </GridItem>
         </Grid>
