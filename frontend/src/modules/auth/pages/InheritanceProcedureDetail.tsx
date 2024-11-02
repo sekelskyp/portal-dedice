21c68e991d4dd88ec5099aed39f4639cea5e5cea
nura16 <nura16@vse.cz>
2024-11-02 12:47:29 +0100

    use correct gql import

--- frontend/src/modules/auth/pages/InheritanceProcedureDetail.tsx
+++ frontend/src/modules/auth/pages/InheritanceProcedureDetail.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { gql, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
 import { Box, Heading, List, Spinner, Text } from '@chakra-ui/react'
 import { useParams } from 'react-router-dom'
 
+import { gql } from '@frontend/gql'
 import { Page } from '@frontend/shared/layout'
 
-const GET_PROCEDURE_QUERY = gql`
+const GET_PROCEDURE_QUERY = gql(/* GraphQL */ `
   query GetProcedureById($id: Int!) {
     getProcedureById(id: $id) {
       id
@@ -49,7 +50,7 @@ const GET_PROCEDURE_QUERY = gql`
       state
     }
   }
-`
+`)
 
 const InheritanceProcedureDetail: React.FC = () => {
   const { id } = useParams()
@@ -76,10 +77,9 @@ const InheritanceProcedureDetail: React.FC = () => {
 
   const procedure = data?.getProcedureById
 
-  const totalAssetsValue = procedure?.procedureAssets.reduce(
-    (sum: number, asset: any) => sum + asset.value,
+  const totalAssetsValue =
+    procedure?.procedureAssets?.reduce((sum, asset) => sum + asset.value, 0) ??
     0
-  )
 
   return (
     <Page>
@@ -102,24 +102,24 @@ const InheritanceProcedureDetail: React.FC = () => {
             </Text>
             <Text fontSize="lg">
               <strong>Hlavní kontaktní osoba:</strong>{' '}
-              {procedure.mainBeneficiary.contact.name}{' '}
-              {procedure.mainBeneficiary.contact.surname}
+              {procedure.mainBeneficiary?.contact?.name}{' '}
+              {procedure.mainBeneficiary?.contact?.surname}
             </Text>
             <Heading as="h2" size="lg" mt={5} mb={3}>
               Výpis dědiců
             </Heading>
             <List.Root listStyleType={'none'}>
-              {procedure.beneficiaries.map((beneficiary: any) => (
+              {procedure.beneficiaries?.map((beneficiary) => (
                 <List.Item key={beneficiary.id}>
                   <Text>
-                    <strong>User Email:</strong> {beneficiary.user.email}
+                    <strong>User Email:</strong> {beneficiary.user?.email}
                   </Text>
                   <Text>
-                    <strong>Contact Name:</strong> {beneficiary.contact.name}{' '}
-                    {beneficiary.contact.surname}
+                    <strong>Contact Name:</strong> {beneficiary.contact?.name}{' '}
+                    {beneficiary.contact?.surname}
                   </Text>
                   <Text>
-                    <strong>Contact Email:</strong> {beneficiary.contact.email}
+                    <strong>Contact Email:</strong> {beneficiary.contact?.email}
                   </Text>
                 </List.Item>
               ))}
@@ -132,7 +132,7 @@ const InheritanceProcedureDetail: React.FC = () => {
               Děděné položky
             </Heading>
             <List.Root listStyleType={'none'}>
-              {procedure.procedureAssets.map((asset: any) => (
+              {procedure.procedureAssets?.map((asset) => (
                 <List.Item key={asset.id}>
                   <Text>
                     <strong>Název:</strong> {asset.name}
