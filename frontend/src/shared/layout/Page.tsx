b7f017e2d9441cca6e9d1c49c2d3d7ae022d0af2
Michal Dub <dubm07@vse.cz>
2024-11-02 20:12:00 +0100

    Zrušení paddingu stránky, když kontejner dosáhne maximální šířky.

--- frontend/src/shared/layout/Page.tsx
+++ frontend/src/shared/layout/Page.tsx
@@ -1,9 +1,18 @@
 import { forwardRef } from 'react'
-import { Container, ContainerProps } from '@chakra-ui/react'
+import { Container, ContainerProps, useBreakpoint } from '@chakra-ui/react'
 
 export const Page = forwardRef((props: ContainerProps, ref) => {
+  const breakpoint = useBreakpoint({ breakpoints: ['2xl'] })
+
+  const maxWidthReached = breakpoint === '2xl'
+
   return (
-    <Container maxW="container.xl" py={6} {...props}>
+    <Container
+      maxW="container.xl"
+      py={6}
+      px={maxWidthReached ? 0 : 6}
+      {...props}
+    >
       {props.children}
     </Container>
   )
