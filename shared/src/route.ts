c3c42a3418d4ba513b3d5bd5357f9f8fa6eefb3c
Šimon Kubín <simonkubin77@gmail.com>
2024-11-12 15:21:03 +0100

    vyčištění detailu, přidání asset page + routy

--- shared/src/route.ts
+++ shared/src/route.ts
@@ -15,4 +15,5 @@ export const route = {
   inheritanceProcedure: (id = ':id') => `/portal/inheritance-procedure/${id}`,
   newDocument: (id = ':id') =>
     `/portal/inheritance-procedure/${id}/new-document`,
+  asset: (id = ':id') => `/portal/inheritance-procedure/${id}/assets`,
 }
