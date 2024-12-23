b0dd5d4fff365f2f8a427132fcd5ee2146ad0f2a
posd03 <posd03@vse.cz>
2024-12-23 13:33:23 +0100

    refactor: remove console logs and clean up contact components

--- frontend/src/modules/wizard/components/contact/ContactInfo.tsx
+++ frontend/src/modules/wizard/components/contact/ContactInfo.tsx
@@ -1,12 +1,6 @@
-import { Container, useBreakpointValue } from '@chakra-ui/react'
-import { FiMail, FiMapPin, FiPhone } from 'react-icons/fi'
+import { Container } from '@chakra-ui/react'
 
-import {
-  formatPhoneNumberForDisplay,
-  formatPhoneNumberForLink,
-} from '../../utils/contactUtils'
-
-import { ContactInfoItem, ContactInfoItemProps } from './ContactInfoItem'
+import { ContactInfoItem } from './ContactInfoItem'
 
 interface ContactInfoProps {
   contactInfo: {
@@ -17,32 +11,6 @@ interface ContactInfoProps {
 }
 
 export function ContactInfo({ contactInfo }: ContactInfoProps) {
-  const iconBreakpoints = useBreakpointValue({
-    base: '18px',
-    sm: '20px',
-    md: '24px',
-  })
-
-  const contactIcons: ContactInfoItemProps[] = [
-    {
-      icon: <FiPhone size={iconBreakpoints} />,
-      text: contactInfo.phone
-        ? formatPhoneNumberForLink(contactInfo.phone)
-        : '',
-      displayText: contactInfo.phone
-        ? formatPhoneNumberForDisplay(contactInfo.phone)
-        : '',
-    },
-    {
-      icon: <FiMail size={iconBreakpoints} />,
-      text: contactInfo.email!,
-    },
-    {
-      icon: <FiMapPin size={iconBreakpoints} />,
-      text: contactInfo.completeAddress!,
-    },
-  ]
-
   return (
     <Container
       textAlign="center"
@@ -55,14 +23,11 @@ export function ContactInfo({ contactInfo }: ContactInfoProps) {
       px={{ base: 4, sm: 8, md: 10 }}
       py={4}
     >
-      {contactIcons.map((item, index) => (
-        <ContactInfoItem
-          key={index}
-          icon={item.icon}
-          text={item.text}
-          displayText={item.displayText}
-        />
-      ))}
+      <ContactInfoItem
+        phone={contactInfo.phone}
+        email={contactInfo.email}
+        completeAddress={contactInfo.completeAddress}
+      />
     </Container>
   )
 }
