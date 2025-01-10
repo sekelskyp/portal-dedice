d2a59062d1bba38ff66e0061ab5d3c92ac64b4cc
sekelskyp <sekp02@vse.cz>
2025-01-10 10:18:33 +0100

    Add asset icons

--- frontend/src/modules/inheritance-splitting/components/AssetCard.tsx
+++ frontend/src/modules/inheritance-splitting/components/AssetCard.tsx
@@ -1,6 +1,24 @@
 import { useEffect } from 'react'
-import { Card, Grid, HStack, ListCollection, Text } from '@chakra-ui/react'
+import {
+  Card,
+  Flex,
+  Grid,
+  HStack,
+  ListCollection,
+  Text,
+} from '@chakra-ui/react'
 import { createListCollection } from '@chakra-ui/react/collection'
+import {
+  Banknote,
+  Building2,
+  Car,
+  Clock,
+  CreditCard,
+  Gem,
+  Home,
+  LineChart,
+  PiggyBank,
+} from 'lucide-react'
 import { useFormContext } from 'react-hook-form'
 
 import { SelectFormControl } from '@frontend/shared/forms'
@@ -20,6 +38,30 @@ interface AssetCardProps {
   asset: Asset
   index: number
   getHeirCollection: (isIndivisible: boolean) => ListCollection<Heir>
+}
+
+interface IconMap {
+  [key: string]: JSX.Element
+}
+
+const assetTypeToIcon: IconMap = {
+  'běžný účet': <CreditCard size={32} />,
+  'spořící účet': <PiggyBank size={32} />,
+  'termínovaný vklad': <Clock size={32} />,
+  'spoření na nemovitost': <Building2 size={32} />,
+  'cenné papíry': <LineChart size={32} />,
+  cennosti: <Gem size={32} />,
+  hotovost: <Banknote size={32} />,
+  vozidlo: <Car size={32} />,
+  nemovitost: <Home size={32} />,
+}
+
+interface AssetIconProps {
+  type: string
+}
+
+const AssetIcon = ({ type }: AssetIconProps) => {
+  return assetTypeToIcon[type.toLocaleLowerCase()]
 }
 
 export const AssetCard = ({
@@ -42,8 +84,13 @@ export const AssetCard = ({
   return (
     <Card.Root variant={'elevated'} px={4} py={3}>
       <Card.Body>
-        <Card.Title mb={6}>{asset.type || 'Neuvedeno'}</Card.Title>
-        <Grid templateColumns="repeat(4, 1fr)" rowGap={8}>
+        <Card.Title mb={6}>
+          <Flex align="center" gap={4}>
+            <AssetIcon type={asset.type} />
+            {asset.type || 'Neuvedeno'}
+          </Flex>
+        </Card.Title>
+        <Grid templateColumns="repeat(4, 1fr)" rowGap={8} columnGap={6}>
           <AssetDetailField label="Název" value={asset.name || 'Neuvedeno'} />
           <AssetDetailField
             label="Vlastnictví"
@@ -63,8 +110,10 @@ export const AssetCard = ({
             value={`${asset.value || '0'} Kč`}
           />
           {asset.isShared && (
-            <HStack gridColumn="span 2" gap={2}>
-              <Text whiteSpace="nowrap">Přiřadit k:</Text>
+            <HStack gridColumn="span 2">
+              <Text whiteSpace="nowrap" color="gray.600">
+                Přiřadit k:
+              </Text>
               <SelectFormControl
                 label=""
                 name={`assets.${index}.sharedOwner`}
@@ -74,8 +123,10 @@ export const AssetCard = ({
               />
             </HStack>
           )}
-          <HStack gridColumn="span 2" gap={2}>
-            <Text whiteSpace="nowrap">* Návrh rozdělení:</Text>
+          <HStack gridColumn="span 2">
+            <Text whiteSpace="nowrap" color="gray.600">
+              * Návrh rozdělení:
+            </Text>
             <SelectFormControl
               label=""
               name={`assets.${index}.heir`}
