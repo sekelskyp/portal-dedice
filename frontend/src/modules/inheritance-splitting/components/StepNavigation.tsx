529d1067e12a65d8a53f9527d4e191ab1cc4fa40
sekelskyp <sekp02@vse.cz>
2025-01-15 13:21:31 +0100

    Modify calculation and add dialog to last step

--- frontend/src/modules/inheritance-splitting/components/StepNavigation.tsx
+++ frontend/src/modules/inheritance-splitting/components/StepNavigation.tsx
@@ -1,4 +1,22 @@
-import { Button, HStack } from '@chakra-ui/react'
+import { useState } from 'react'
+import {
+  Button,
+  DialogBackdrop,
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+  HStack,
+  Portal,
+  VStack,
+} from '@chakra-ui/react'
+
+import { RouterNavLink } from '@frontend/shared/navigation/atoms/RouterNavLink'
+import { route } from '@shared/route'
 
 interface StepNavigationProps {
   onPrevious: () => void
@@ -13,6 +31,7 @@ export const StepNavigation = ({
   isFirstStep,
   isLastStep,
 }: StepNavigationProps) => {
+  const [open, setOpen] = useState(false)
   return (
     <HStack justify="space-between" width="100%" mt={4}>
       <Button
@@ -21,7 +40,47 @@ export const StepNavigation = ({
       >
         Předchozí krok
       </Button>
-      <Button onClick={onNext}>{isLastStep ? 'Dokončit' : 'Další krok'}</Button>
+      {isLastStep ? (
+        <DialogRoot
+          placement="bottom"
+          lazyMount
+          open={open}
+          onOpenChange={(e) => setOpen(e.open)}
+        >
+          <DialogTrigger asChild>
+            <Button onClick={onNext}>Dokončit</Button>
+          </DialogTrigger>
+          <Portal>
+            <DialogBackdrop>
+              <DialogContent>
+                <VStack>
+                  <DialogHeader>
+                    <DialogTitle>Děkujeme za využití </DialogTitle>
+                  </DialogHeader>
+                  <DialogBody>
+                    <p>
+                      Pokud chcete, můžete modelaci udělat znovu a jinak. Pokud
+                      si přejete přejít jinam, klikněte na tlačítko "Zpět na
+                      domovskou stránku"
+                    </p>
+                  </DialogBody>
+                  <DialogFooter>
+                    <RouterNavLink to={route.home()}>
+                      Zpět na domovskou stránku
+                    </RouterNavLink>
+                    <RouterNavLink to={route.inheritance()}>
+                      Zkusit další vypořádaní
+                    </RouterNavLink>
+                  </DialogFooter>
+                  <DialogCloseTrigger />
+                </VStack>
+              </DialogContent>
+            </DialogBackdrop>
+          </Portal>
+        </DialogRoot>
+      ) : (
+        <Button onClick={onNext}>{'Další krok'}</Button>
+      )}
     </HStack>
   )
 }
